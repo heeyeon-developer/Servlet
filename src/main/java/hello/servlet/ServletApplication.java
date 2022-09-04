d11646cb8b81c8ef62b8577b0976defcc4aa97ca
heeyeonkim <heeyeon3472@gmail.com>
2022-09-04 23:43:28 +0900

    test servlet

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -2,7 +2,9 @@ package hello.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan //서블릿 자동 등록 : 현재 패키지의 하위에 존재하는 서블릿을 찾아 실행시켜 주는 annotation
 @SpringBootApplication
 public class ServletApplication {
 
